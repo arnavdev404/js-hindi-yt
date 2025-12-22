096a0a12e0055e29417ecb9d98b6893849e9c755
Arnav <devwitharnav@gmail.com>
2025-12-22 23:26:28 +0000

    commit

--- 01Basics/01variables.js
+++ 01Basics/01variables.js
@@ -1,8 +1,16 @@
 const accountId =144538
 let accountEmail ="anandarnav36@gmail.com"
 var accountPassword="1234"
-acsountCity ="Jaipur"
+accountCity ="Jaipur"
+const accountState  
 
 // accountId=2//not allowed 
+accountEmail= "akriti@123"
+accountPassword="25432"
+accountCity= "lucknow"
 
 console.log(accountId);
+/*
+Prefer Not to Use Var
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
